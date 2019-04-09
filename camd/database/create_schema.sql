65164a3117518da80e27939f59afcff6b05f2aa8
daniel-schweigert-ctr <danielschweigert@MBP15-DSchweigert.local>
2019-04-09 11:07:26 -0700

    add more versatile feature structure

--- camd/database/create_schema.sql
+++ camd/database/create_schema.sql
@@ -1,6 +1,5 @@
 --- Creates the CAMD database schema
 
-DROP SCHEMA camd CASCADE
 CREATE SCHEMA IF NOT EXISTS camd;
 
 
@@ -9,7 +8,7 @@ CREATE TABLE IF NOT EXISTS camd.material (
     id INT PRIMARY KEY,
     definition JSON,
     poscar TEXT,
-    defintion_dft JSON,
+    definition_dft JSON,
     poscar_dft TEXT
 );
 
@@ -20,7 +19,9 @@ CREATE TABLE IF NOT EXISTS camd.material_set (
 
 CREATE TABLE IF NOT EXISTS camd.feature (
     id SERIAL PRIMARY KEY,
-    "name" VARCHAR
+    "name" VARCHAR,
+    "type" VARCHAR,
+    possible_values VARCHAR[]
 );
 
 CREATE TABLE IF NOT EXISTS camd.feature_set (
@@ -29,10 +30,10 @@ CREATE TABLE IF NOT EXISTS camd.feature_set (
 );
 
 CREATE TABLE IF NOT EXISTS camd.featurization (
-    id SERIAL PRIMARY KEY,
     material_id INT REFERENCES camd.material(id),
     feature_id INT REFERENCES camd.feature(id),
-    value DECIMAL
+    value DECIMAL[],
+    PRIMARY KEY (material_id, feature_id)
 );
 
 CREATE TABLE IF NOT EXISTS camd.algorithm (
@@ -56,9 +57,8 @@ CREATE TABLE IF NOT EXISTS camd.dft_runs (
 
 CREATE TABLE IF NOT EXISTS camd.mlmodel (
     id SERIAL PRIMARY KEY,
-    material_set_id INT REFERENCES camd.material_set(id),
-    feature_set_id INT REFERENCES camd.feature_set(id),
     algorithm_id INT REFERENCES camd.algorithm(id),
+    feature_set_id INT REFERENCES camd.feature_set(id),
     train_set_id INT REFERENCES camd.material_set(id),
     dev_set_id INT REFERENCES camd.material_set(id),
     date_time_fit TIMESTAMP WITH TIME ZONE,
