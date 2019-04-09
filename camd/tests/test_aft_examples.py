e8e4db0e35db23e86d4851a18b30a842a302f115
Joseph Montoya <joseph.montoya@tri.global>
2019-04-08 19:11:16 -0700

    add skip back

--- camd/tests/test_aft_examples.py
+++ camd/tests/test_aft_examples.py
@@ -15,8 +15,8 @@ CAMD_LONG_TESTS = os.environ.get("CAMD_LONG_TESTS", False)
 SKIP_MSG = "Long tests disabled, set CAMD_LONG_TESTS to run long tests"
 
 
-# TODO: s3 sync doesn't work on jenkins
-# @unittest.skipUnless(CAMD_LONG_TESTS, SKIP_MSG)
+# TODO: s3 sync doesn't currently work on jenkins
+@unittest.skipUnless(CAMD_LONG_TESTS, SKIP_MSG)
 class AftLoopTestLong(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
