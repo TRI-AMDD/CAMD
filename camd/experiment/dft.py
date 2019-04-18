ec6f078b27c10b1a7bef840b3439e3bf5e63d496
Joseph Montoya <joseph.montoya@tri.global>
2019-04-17 17:05:55 -0700

    add placeholder for dft

---
+++ camd/experiment/dft.py
@@ -0,0 +1,21 @@
+def get_dft_calcs_from_northwestern(uids):
+    """
+    Placeholder function for fetching DFT calculations from Northwestern
+    """
+    pass
+
+
+def get_dft_calcs_from_MC1(uids):
+    """
+    Placeholder function for fetching DFT calculations from MC1
+    """
+    pass
+
+
+def get_dft_calcs_aft(uids, df):
+    """
+    Mock function that mimics fetching DFT calculations
+    """
+    uids = [uids] if type(uids) != list else uids
+    return df.loc[uids]
+
