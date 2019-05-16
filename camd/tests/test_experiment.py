6ad946c22ad3db26124e54dc17f4ed8ab48f8a24
Joseph Montoya <joseph.montoya@tri.global>
2019-05-16 07:19:30 -0700

    try larger test

--- camd/tests/test_experiment.py
+++ camd/tests/test_experiment.py
@@ -3,15 +3,17 @@ import time
 
 from monty.tempfile import ScratchDir
 from pymatgen.util.testing import PymatgenTest
+from pymatgen import MPRester
 from camd.experiment import submit_dft_calcs_to_mc1, check_dft_calcs,\
     run_dft_experiments
 
 
 # This test is still inconsistent because of issues with
 # batch AWS jobs and database communications
-@unittest.skipIf(False, "toggle this test")
 class Mc1Test(unittest.TestCase):
+    @unittest.skipIf(True, "toggle this test")
     def test_get(self):
+
         good_silicon = PymatgenTest.get_structure("Si")
         bad_silicon = good_silicon.copy()
 
@@ -25,6 +27,20 @@ class Mc1Test(unittest.TestCase):
 
         self.assertEqual(calc_status['good']['status'], 'SUCCEEDED')
         self.assertEqual(calc_status['bad']['status'], 'FAILED')
+
+    @unittest.skipIf(False, "toggle this test")
+    def test_structure_suite(self):
+        # TODO: fix the formation energy calculation
+        mp_ids = ["mp-702",
+                  "mp-1953",
+                  "mp-1132",
+                  "mp-8409",
+                  "mp-872"]
+        with MPRester() as mpr:
+            structure_dict = {mp_id: mpr.get_structure_by_material_id(mp_id)
+                              for mp_id in mp_ids}
+        status = run_dft_experiments(structure_dict)
+        self.assertTrue(all([run['status'] == "SUCCEEDED" for run in status]))
 
 
 if __name__ == '__main__':
