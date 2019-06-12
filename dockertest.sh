098f4d48934cf79590091fbefa506369e25c2a3f
Joseph Montoya <joseph.montoya@tri.global>
2019-06-12 15:24:59 -0400

    update bash control in dockertest script

--- dockertest.sh
+++ dockertest.sh
@@ -3,8 +3,7 @@
 # This script is run as the default procedure for testing
 # in the docker container
 
-# turn on bash's job control
-set -m
+set -e
 
 # TODO: set up postgresql testing
 # Start postgres in the background
