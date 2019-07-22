a49f3a164be876c000b6ec28bac75e1300160941
Joseph Montoya <joseph.montoya@tri.global>
2019-07-22 15:00:17 -0700

    update error handling and syncing

--- camd/campaigns/structure_discovery.py
+++ camd/campaigns/structure_discovery.py
@@ -11,7 +11,8 @@ from camd.agent.base import RandomAgent
 from camd.analysis import AnalyzeStability
 from camd.experiment import ATFSampler
 from camd.loop import Loop
-from camd import CAMD_TEST_FILES
+from camd import CAMD_TEST_FILES, CAMD_S3_BUCKET
+from camd.utils.s3 import s3_sync
 
 from camd.analysis import AnalyzeStability_mod
 from camd.experiment.dft import OqmdDFTonMC1
@@ -32,36 +33,38 @@ def run_proto_dft_campaign(chemsys):
         (bool): True if run exits
 
     """
-    # Get structure domain
-    element_list = chemsys.split('-')
-    domain = StructureDomain.from_bounds(
-        element_list, n_max_atoms=12, **{'grid': range(1, 3)})
-    candidate_data = domain.candidates()
-    structure_dict = domain.hypo_structures_dict
+    s3_prefix = "proto-dft/runs/{}".format(chemsys)
 
-    # Dump structure/candidate data
-    with open('candidate_data.pickle', 'wb') as f:
-        pickle.dump(candidate_data, f)
-    with open('structure_dict.pickle', 'wb') as f:
-        pickle.dump(structure_dict, f)
-
-    # Set up agents and loop parameters
-    agent = AgentStabilityML5  # Query-by-committee agent that operates with maximum expected gain
-    agent_params = {
-        'ML_algorithm': MLPRegressor,  # We'll use a simple NN regressor
-        'ML_algorithm_params': {'hidden_layer_sizes': (84, 50)},
-        'N_query': 10,  # Number of experiments the agent can request in each round.
-        'hull_distance': 0.25,  # Distance to hull to consider a finding as discovery (eV/atom)
-        'frac': 0.7  # Fraction to exploit
-    }
-    analyzer = AnalyzeStability_mod
-    analyzer_params = {'hull_distance': 0.2}  # analysis criterion (need not be exactly same as agent's goal)
-    experiment = OqmdDFTonMC1
-    experiment_params = {'structure_dict': structure_dict, 'candidate_data': candidate_data, 'timeout': 20000}
-    experiment_params.update({'timeout': 30000})
-
-    # Construct and start loop
     try:
+        # Get structure domain
+        element_list = chemsys.split('-')
+        domain = StructureDomain.from_bounds(
+            element_list, n_max_atoms=12, **{'grid': range(1, 3)})
+        candidate_data = domain.candidates()
+        structure_dict = domain.hypo_structures_dict
+
+        # Dump structure/candidate data
+        with open('candidate_data.pickle', 'wb') as f:
+            pickle.dump(candidate_data, f)
+        with open('structure_dict.pickle', 'wb') as f:
+            pickle.dump(structure_dict, f)
+
+        # Set up agents and loop parameters
+        agent = AgentStabilityML5  # Query-by-committee agent that operates with maximum expected gain
+        agent_params = {
+            'ML_algorithm': MLPRegressor,  # We'll use a simple NN regressor
+            'ML_algorithm_params': {'hidden_layer_sizes': (84, 50)},
+            'N_query': 10,  # Number of experiments the agent can request in each round.
+            'hull_distance': 0.25,  # Distance to hull to consider a finding as discovery (eV/atom)
+            'frac': 0.7  # Fraction to exploit
+        }
+        analyzer = AnalyzeStability_mod
+        analyzer_params = {'hull_distance': 0.2}  # analysis criterion (need not be exactly same as agent's goal)
+        experiment = OqmdDFTonMC1
+        experiment_params = {'structure_dict': structure_dict, 'candidate_data': candidate_data, 'timeout': 20000}
+        experiment_params.update({'timeout': 30000})
+
+        # Construct and start loop
         new_loop = Loop(
             candidate_data, agent, experiment, analyzer, agent_params=agent_params,
             analyzer_params=analyzer_params, experiment_params=experiment_params,
@@ -73,7 +76,7 @@ def run_proto_dft_campaign(chemsys):
         error_msg = {"error": "{}".format(e),
                      "traceback": traceback.format_exc()}
         dumpfn(error_msg, "error.json")
-        new_loop.s3_sync()
+        s3_sync(s3_bucket=CAMD_S3_BUCKET, s3_prefix=s3_prefix, sync_path='.')
 
     return True
 
