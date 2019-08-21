4a90993f4775a7eebe44542093d168882ba42fa2
Muratahan Aykol <murat.aykol@tri.global>
2019-08-21 14:02:48 -0700

    atf examples refreshed

--- camd/atf_examples/agent_qbc_v2/agent_qbc_v2.py
+++ camd/atf_examples/agent_qbc_v2/agent_qbc_v2.py
@@ -10,26 +10,28 @@ from camd.utils.s3 import cache_s3_objs
 from camd import S3_CACHE
 import os
 
-cache_s3_objs(['camd/shared-data/oqmd_voro_March25_v2.csv'])
+cache_s3_objs(['camd/shared-data/oqmd_1.2_voronoi_magpie_fingerprints.pickle'])
 ##########################################################
 # Binary stable material discovery QBC based agent recipe
 ##########################################################
-df = pd.read_csv(os.path.join(S3_CACHE, 'camd/shared-data/oqmd_voro_March25_v2.csv')).sample(frac=0.2)
-N_seed = 5000  # Starting sample size
+df = pd.read_pickle(os.path.join(S3_CACHE,
+                              'camd/shared-data/oqmd_1.2_voronoi_magpie_fingerprints.pickle')).sample(frac=0.2)
+N_seed = 5000  # Starting sample size - a seed of this size will be randomly chosen.
 N_query = 200  # This many new candidates are "calculated with DFT" (i.e. requested from Oracle -- DFT)
 agent = QBCStabilityAgent
 agent_params = {
     'ML_algorithm': MLPRegressor,
     'ML_algorithm_params': {'hidden_layer_sizes': (84, 50)},
-    'N_query': N_query, 'N_species': 2,
-    'N_members': 10,  # Committee size
+    'N_query': N_query,
+    'N_species': 2,         # We'll restrict the search to binaries
+    'N_members': 10,        # Committee size in QBC
     'hull_distance': 0.05,  # Distance to hull to consider a finding as discovery (eV/atom)
-    'frac': 0.5
+    'frac': 0.5             # Fraction of data to choose to form a committee member
     }
 analyzer = AnalyzeStability_mod
 analyzer_params = {'hull_distance': 0.05}
 experiment = ATFSampler
-experiment_params = {"params": {'dataframe': df}}
+experiment_params = {'dataframe': df}
 candidate_data = df
 ##########################################################
 new_loop = Loop(candidate_data, agent, experiment, analyzer,
