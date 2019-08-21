54e64cae7ae276416845b035ae2ad7cf0336eb1d
Muratahan Aykol <murat.aykol@tri.global>
2019-08-21 15:18:34 -0700

    gp test

--- camd/tests/test_atf_examples.py
+++ camd/tests/test_atf_examples.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from sklearn.neural_network import MLPRegressor
 from pymatgen import Composition
-from camd.agent.agents import QBCStabilityAgent
+from camd.agent.agents import QBCStabilityAgent, GaussianProcessStabilityAgent
 from camd.agent.base import RandomAgent
 from camd.analysis import AnalyzeStability_mod as AnalyzeStability
 from camd.experiment import ATFSampler
@@ -124,6 +124,34 @@ class AtfLoopTest(unittest.TestCase):
             'N_members': 10,  # Committee size
             'hull_distance': 0.05,  # Distance to hull to consider a finding as discovery (eV/atom)
             'frac': 0.5  # Fraction to exploit (rest will be explored -- randomly picked)
+        }
+        analyzer = AnalyzeStability
+        analyzer_params = {'hull_distance': 0.05}
+        experiment = ATFSampler
+        experiment_params = {'dataframe': df_sub}
+        candidate_data = df_sub
+        path = '.'
+
+        new_loop = Loop(candidate_data, agent, experiment, analyzer,
+                        agent_params=agent_params, analyzer_params=analyzer_params,
+                        experiment_params=experiment_params,
+                        create_seed=n_seed)
+        new_loop.initialize()
+        self.assertTrue(new_loop.initialized)
+
+        new_loop.auto_loop(6)
+        self.assertTrue(True)
+
+    def test_simple_gp_loop(self):
+        df = pd.read_csv(os.path.join(CAMD_TEST_FILES, 'test_df.csv'))
+        df_sub = df[df['N_species'] <= 3]
+        n_seed = 200  # Starting sample size
+        n_query = 10  # This many new candidates are "calculated with DFT" (i.e. requested from Oracle -- DFT)
+        agent = GaussianProcessStabilityAgent
+        agent_params = {
+            'N_query': n_query,
+            'hull_distance': 0.05,  # Distance to hull to consider a finding as discovery (eV/atom)
+            'alpha': 0.5  # Fraction of std to include in expected improvement
         }
         analyzer = AnalyzeStability
         analyzer_params = {'hull_distance': 0.05}
