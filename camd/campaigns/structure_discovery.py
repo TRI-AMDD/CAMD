dc479f436e8c8bc6c098ae4e85b7b77991d420bf
Muratahan Aykol <murat.aykol@tri.global>
2019-09-12 16:55:55 -0700

    more flexible definition of bounds

--- camd/campaigns/structure_discovery.py
+++ camd/campaigns/structure_discovery.py
@@ -124,7 +124,7 @@ def run_atf_campaign(chemsys):
     return True
 
 
-def n_max_iter_heuristics(n_data, n_query):
+def n_max_iter_heuristics(n_data, n_query, low_bound=5, up_bound=20):
     """
     Helper method to define maximum number of iterations for a given campaign.
     This is based on the empirical evidence in various systems >90% of stable
@@ -134,11 +134,13 @@ def n_max_iter_heuristics(n_data, n_query):
     Args:
         n_data (int): number of data points in candidate space
         n_query (int): number of queries allowed in each iteration
+        low_bound (int): lower bound allowed for n_max_iter
+        up_bound (int): upper bound allowed for n_max_ite
     Returns:
         maximum number of iterations as integer
     """
     _target = round(n_data*0.25/n_query)
-    if _target<5:
-        return 5
+    if _target<low_bound:
+        return low_bound
     else:
-        return min(_target, 20)
+        return min(_target, up_bound)
