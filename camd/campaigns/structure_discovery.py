7338d2e55b7097a0183a1eccbec302871dbbf546
Muratahan Aykol <murat.aykol@tri.global>
2019-09-16 21:17:40 -0700

    new agent and parameters for dft campaign

--- camd/campaigns/structure_discovery.py
+++ camd/campaigns/structure_discovery.py
@@ -7,21 +7,20 @@ import os
 from datetime import datetime
 from monty.serialization import dumpfn
 from camd.domain import StructureDomain, heuristic_setup
-from camd.agent.agents import AgentStabilityML5
+from camd.agent.agents import AgentStabilityAdaBoost
 from camd.agent.base import RandomAgent
-from camd.analysis import AnalyzeStability
 from camd.experiment import ATFSampler
 from camd.loop import Loop
 from camd import CAMD_TEST_FILES, CAMD_S3_BUCKET
 from camd.utils.s3 import s3_sync
 
-from camd.analysis import AnalyzeStability_mod
+from camd.analysis import AnalyzeStability_mod, FinalizeQqmdCampaign
 from camd.experiment.dft import OqmdDFTonMC1
 from sklearn.neural_network import MLPRegressor
 import pickle
 
 
-__version__ = "2019.09.12"
+__version__ = "2019.09.16"
 
 
 # TODO: abstract campaign?
@@ -60,25 +59,31 @@ def run_proto_dft_campaign(chemsys):
             pickle.dump(structure_dict, f)
 
         # Set up agents and loop parameters
-        agent = AgentStabilityML5  # Query-by-committee agent that operates with maximum expected gain
+        agent = AgentStabilityAdaBoost
         agent_params = {
-            'ML_algorithm': MLPRegressor,  # We'll use a simple NN regressor
+            'ML_algorithm': MLPRegressor,
             'ML_algorithm_params': {'hidden_layer_sizes': (84, 50)},
-            'N_query': 10,  # Number of experiments the agent can request in each round.
-            'hull_distance': 0.25,  # Distance to hull to consider a finding as discovery (eV/atom)
-            'frac': 0.7  # Fraction to exploit
+            'N_query': 10,
+            'hull_distance': 0.2,  # Distance to hull to consider a finding as discovery (eV/atom)
+            'frac': 1.0,  # Fraction to exploit (rest will be explored -- randomly picked)
+            'uncertainty': True,
+            'alpha': 0.5,
+            'n_estimators': 20
         }
         analyzer = AnalyzeStability_mod
         analyzer_params = {'hull_distance': 0.2}  # analysis criterion (need not be exactly same as agent's goal)
         experiment = OqmdDFTonMC1
         experiment_params = {'structure_dict': structure_dict, 'candidate_data': candidate_data, 'timeout': 30000}
         experiment_params.update({'timeout': 30000})
+        finalizer = FinalizeQqmdCampaign
+        finalizer_params = {'hull_distance': 0.2}
         n_max_iter = n_max_iter_heuristics(len(candidate_data), 10)
 
         # Construct and start loop
         new_loop = Loop(
             candidate_data, agent, experiment, analyzer, agent_params=agent_params,
             analyzer_params=analyzer_params, experiment_params=experiment_params,
+            finalizer=finalizer, finalizer_params=finalizer_params,
             s3_prefix="proto-dft/runs/{}".format(chemsys))
         new_loop.auto_loop_in_directories(
             n_iterations=n_max_iter, timeout=10, monitor=True,
