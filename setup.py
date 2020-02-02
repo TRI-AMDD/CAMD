cdcf4bc53bf310447ceed08c76f2213a58b32fb9
Joseph Montoya <joseph.montoya@tri.global>
2020-02-01 16:01:50 -0800

    try in setup requires

--- setup.py
+++ setup.py
@@ -19,7 +19,9 @@ setup(
     author_email='murat.aykol@tri.global',
     description='',
     setup_requires=["numpy==1.18",
-                    "Django==2.2"],  # For qmpy depnedency
+                    "Django==2.2",
+                    "tensorflow==1.15.0",
+                    "gpflow==1.5.0"],  # For qmpy depnedency
     install_requires=["numpy==1.18",
                       "python-dateutil==2.8.0",
                       "networkx==2.2",
@@ -34,8 +36,6 @@ setup(
                       "autologging",
                       "awscli==1.16.199",
                       "docopt==0.6.2",
-                      "tensorflow==1.15.0",
-                      "gpflow==1.5.0"
                       ],
     # TODO: make this materials?
     dependency_links=[
