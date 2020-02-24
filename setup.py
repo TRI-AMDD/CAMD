96ef8e74ae650badc5a55655ef58a7b3b8bf29d0
Joseph Montoya <joseph.montoya@tri.global>
2020-02-24 12:28:30 -0800

    modify setup, reqs

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     setup_requires=["numpy==1.18",
-                    "Django==2.2",
-                    "tensorflow==1.15.0",
+                    "Django==2.2.10",
+                    "tensorflow==1.15.2",
                     "gpflow==1.5.0"],  # For qmpy depnedency
     install_requires=["numpy==1.18",
                       "python-dateutil==2.8.0",
@@ -30,10 +30,9 @@ setup(
                       "matplotlib==3.1.1",
                       "qmpy",  # This version is constrained by the source
                       "pandas==0.24.2",
-                      "boto3==1.9.136",
                       "matminer==0.5.5",
                       "autologging",
-                      "awscli==1.16.199",
+                      "awscli>=1.16.199",
                       "docopt==0.6.2",
                       ],
     extras_require={
@@ -45,8 +44,8 @@ setup(
                   "matplotlib"]
     },
     dependency_links=[
-        "http://github.com/JosephMontoya-TRI/qmpy_py3/tarball/master#egg=qmpy",
-        "http://github.com/ToyotaResearchInstitute/protosearch/tarball/master#egg=protosearch",
+        "https://github.com/JosephMontoya-TRI/qmpy_py3/tarball/master#egg=qmpy",
+        "https://github.com/ToyotaResearchInstitute/protosearch/tarball/master#egg=protosearch",
     ],
     entry_points={
         "console_scripts": [
