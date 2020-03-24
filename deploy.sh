217126657607f993823b0b1181597d1149d33fae
Joseph Montoya <joseph.montoya@tri.global>
2020-03-23 17:37:16 -0700

    add conditional for travis

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@ set -e
 
 export DOCKER_TAG=camd-public
 
-docker build -t $DOCKER_TAG .
-docker tag $DOCKER_TAG:latest $DOCKER_URI:latest
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $DOCKER_URI
-docker push $DOCKER_URI:latest
+if [[ "$TRAVIS_BRANCH" =~ ^(master|dev|update_travis)$ ]]; then
+    docker build -t $DOCKER_TAG .
+    docker tag $DOCKER_TAG:latest $DOCKER_URI:$TRAVIS_BRANCH-latest
+    aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $DOCKER_URI
+    docker push $DOCKER_URI:$TRAVIS_BRANCH-latest
+else
+    echo "TRAVIS_BRANCH $TRAVIS_BRANCH not in push list"
+fi
