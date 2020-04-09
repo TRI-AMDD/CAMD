1047585a886ff706f7bd8b62b41b37195c9704e0
Joseph Montoya <joseph.montoya@tri.global>
2020-04-09 00:51:10 -0700

    add scratch functionality

--- camd/campaigns/runner.py
+++ camd/campaigns/runner.py
@@ -3,9 +3,10 @@
 Module and script for running the CAMD campaign runner
 
 Usage:
-    camd_runner CAMPAIGN_PREFIX
+    camd_runner CAMPAIGN_PREFIX [options]
 
 Options:
+    --scratch            run in scratch directry
     -h --help            Show this screen
     --version            Show version
 
@@ -13,6 +14,10 @@ CAMPAIGN_PREFIX should be a "reserved name" for a campaign,
 i.e. '/'-delimited string beginning with the campaign type
 and reserved name, e. g. meta-agent/test-1
 """
+
+import os
+import tempfile
+import shutil
 from docopt import docopt
 from camd.campaigns.meta_agent import MetaAgentCampaign
 
@@ -22,6 +27,11 @@ def main():
     args = docopt(__doc__)
     campaign_prefix = args['CAMPAIGN_PREFIX']
     campaign_type, reserved_name = campaign_prefix.split('/', 1)
+    cwd = os.getcwd()
+    if args['--scratch']:
+        dirpath = tempfile.mkdtemp()
+        os.chdir(dirpath)
+
     # Switch for different campaign types
     if campaign_type == "meta_agent":
         campaign = MetaAgentCampaign.from_reserved_name(reserved_name)
@@ -30,6 +40,11 @@ def main():
             campaign_type))
 
     campaign.autorun()
+
+    # Cleanup
+    if args['--scratch']:
+        os.chdir(cwd)
+        shutil.rmtree(dirpath)
 
 
 if __name__ == "__main__":
