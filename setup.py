80fb77275e1bfe8ff45ce3e46581dbb562f8355d
Joseph Montoya <joseph.montoya@tri.global>
2020-05-19 19:15:24 -0700

    v2020.5.19 release

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ based on ongoing work led by the
 [Toyota Research Institute](http://www.tri.global/accelerated-materials-design-and-discovery/).
 
 camd enables the construction of sequential learning pipelines using a set of
-abstractions that include 
-* Agents - decision making entities which select experiments to run from pre-determined 
+abstractions that include
+* Agents - decision making entities which select experiments to run from pre-determined
     candidate sets
-* Experiments - experimental procedures which augment candidate data in a way that 
+* Experiments - experimental procedures which augment candidate data in a way that
     facilitates further experiment selection
-* Analyzers - Post-processing procedures which frame experimental results in the context 
+* Analyzers - Post-processing procedures which frame experimental results in the context
     of candidate or seed datasets
 
 In addition to these abstractions, camd provides a loop construct which executes
@@ -34,7 +34,7 @@ after the fact sampling of known data.
 setup(
     name='camd',
     url="https://github.com/TRI-AMDD/CAMD",
-    version="2020.4.28-dev",
+    version="2020.5.19",
     packages=find_packages(),
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
