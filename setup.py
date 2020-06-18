05ab96d898526053b8bd01f0b1a34c221c423a5a
Joseph Montoya <joseph.montoya@tri.global>
2020-06-18 16:29:15 +0000

    update setup

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
                       "pandas==1.0.1",
                       "matminer==0.6.3",
                       "autologging",
-                      "awscli==1.18.28",
+                      "awscli==1.18.82",
                       "docopt==0.6.2",
                       "boto3==1.12.28",
                       "scikit-learn==0.22.1",
