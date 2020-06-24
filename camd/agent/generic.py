7a189fecefef29b6a8474fa9b4f91550d012f1f2
Muratahan Aykol <murat.aykol@tri.global>
2020-06-23 21:49:11 -0700

    multi-purpose GPy-bsed batch-mode agent v1

--- camd/agent/generic.py
+++ camd/agent/generic.py
@@ -1,5 +1,6 @@
 import numpy as np
 import gpflow
+import GPy
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import KFold, cross_val_score
@@ -69,13 +70,13 @@ class GPBatchUCB(HypothesisAgent):
     Generic Gaussian Process (GP) and upper confidence bound (UCB)
     based agent that tries to maximize a target that can
     be used in a batch-mode Bayesian Optimization setting.
-
-    The implementation is based on Desaut et al. ICML 2014 and Srivinas et al. 2010
-
+    This class implements a version of the Batch-UCB algorithm described by
+    Desautels et al. Journal of Machine Learning Research 15 (2014) 4053-4103
+    which shares the same basis with Srinivas et al. arXiv preprint arXiv:0912.3995 (2009).
     """
 
     def __init__(
-            self, candidate_data=None, seed_data=None, n_query=None, alpha=1.0, kernel=None, **kwargs
+            self, candidate_data=None, seed_data=None, n_query=None, mode='batch', alpha=1.0, kernel=None, **kwargs
     ):
         """
         Args:
@@ -83,30 +84,35 @@ class GPBatchUCB(HypothesisAgent):
                     and at least one additional column that can be used as descriptors.
             seed_data (pandas.DataFrame):  data which to fit the Agent to.
             n_query (int): number of queries in allowed. Defaults to 1.
+            mode (str): "batch" or "naive"; corresponding to original BUCB algorithm and a naive batch algorithm
+                where batch is composed in one-shot based on ranking. "naive" is mostly for benchmarking, but is also
+                faster so might be useful large, limiting dataset sizes.
             alpha (float or str): mixing parameter for uncertainties in UCB. If a float is given, agent will
                 use the same constant alpha throughout the campaign. Defaults to 1.0. Setting this as 'auto' will
                 use the Theorem 1 from Srivanasan et al. to determine the alpha during batch composition.
                 'auto' has two parameters, 'delta' and 'premultip', which default to 0.1 and 0.05 respectively,
-                but can be modified by passing as kwargs to the agent.
+                but can be modified by passing as kwargs to the agent. if mode is "naive" this can only be a flaot.
             kernel (scikit-learn kernel): Kernel object for the GP. Defaults to ConstantKernel(1.0)*RBF(1.0).
                     See scikit-learn.gaussian_process.kernels for details.
         """
         self.candidate_data = candidate_data
         self.seed_data = seed_data
         self.n_query = n_query if n_query else 1
-        self.cv_score = np.inf
+        self.mode = mode
         self.alpha = alpha
-        self.kernel = kernel if kernel else gpflow.kernels.RBF()
+        self.kernel = kernel
         self.kwargs = kwargs
-
-        self._delta = kwargs.get('delta', 0.1)
-        self._premultip = kwargs.get('premultip', 0.05)
 
         super(GPBatchUCB).__init__()
 
     def get_hypotheses(self, candidate_data, seed_data=None):
         self.candidate_data = candidate_data.drop(columns=["target"], axis=1)
         self.seed_data = seed_data
+
+        fb_start = max(len(self.seed_data) - len(self._initial_seed_indices), 1)
+
+        if self.kernel is None:
+            self.kernel = GPy.kern.RBF(input_dim=self.candidate_data.shape[1])
 
         X_seed = seed_data.drop(columns=["target"], axis=1)
         y_seed = seed_data["target"].to_numpy().reshape(-1, 1)
@@ -119,38 +125,58 @@ class GPBatchUCB(HypothesisAgent):
 
         r_seed, r_y, r_candidates = X_seed, y_seed, self.candidate_data
 
-        batch = []
-        for i in range(min(self.n_query, len(self.candidate_data))):
+        if self.mode is "batch":
+            batch = []
+            for i in range(min(self.n_query, len(self.candidate_data))):
+                x = scaler.transform(r_seed).astype(np.float64)
+                y = r_y.astype(np.float64).reshape(-1, 1)
+
+                m = GPy.models.GPRegression(x, y,
+                                            kernel=self.kernel,
+                                            noise_var=self.kwargs.get('noise_var', 1.0))
+                m.optimize(optimizer=self.kwargs.get('optimizer', 'bfgs'),
+                           max_iters=self.kwargs.get('max_iters', 1000))
+                self.kernel = m.kern
+
+                y_pred, var = m.predict(scaler.transform(r_candidates.to_numpy().astype(np.float64)))
+                t_pred = y_pred * y_std + y_m
+                unc = np.sqrt(var) * y_std + y_m
+
+                if self.alpha == 'auto':
+                    _t = i + fb_start
+                    alpha = self.kwargs.get('premultip', 0.05) * np.sqrt(
+                        2 * np.log(len(self.candidate_data) * _t ** 2 * np.pi ** 2 / 6 / self.kwargs.get('delta', 0.1)))
+                    print(alpha)
+                else:
+                    alpha = self.alpha
+
+                t_pred += unc * alpha
+                s = np.argmax(t_pred)
+
+                name = r_candidates.index.tolist()[s]
+                print(s, (y_pred[s] * y_std + y_m)[0], t_pred[s][0], unc[s], candidate_data['target'].loc[name])
+                batch.append(name)
+                print(batch, candidate_data.loc[batch]['target'])
+                r_seed = r_seed.append(r_candidates.loc[name])
+                r_y = np.append(r_y, np.array([y_pred[s]]).reshape(1, 1), axis=0)
+                r_candidates = r_candidates.drop(name)
+
+        elif self.mode is "naive":
             x = scaler.transform(r_seed).astype(np.float64)
             y = r_y.astype(np.float64).reshape(-1, 1)
-
-            m = gpflow.models.GPR((x, y), kernel=self.kernel)
-            m.likelihood.variance.assign(0.1)
-
-            opt = gpflow.optimizers.Scipy()
-            opt.minimize(m.training_loss,
-                                    m.trainable_variables, options=dict(maxiter=200))
-
-            self.kernel = m.kernel
-            pred, var = m.predict_f(scaler.transform(r_candidates.to_numpy().astype(np.float64)))
-            t_pred = pred * y_std + y_m
+            m = GPy.models.GPRegression(x, y,
+                                        kernel=self.kernel,
+                                        noise_var=self.kwargs.get('noise_var', 1.0))
+            self.kernel = m.kern
+            m.optimize(optimizer=self.kwargs.get('optimizer', 'bfgs'),
+                       max_iters=self.kwargs.get('max_iters', 1000))
+            y_pred, var = m.predict(scaler.transform(r_candidates.to_numpy().astype(np.float64)))
+            t_pred = y_pred * y_std + y_m
             unc = np.sqrt(var) * y_std + y_m
+            t_pred += unc * self.alpha
+            indices = np.argsort(-1.0 * t_pred.flatten())[: self.n_query]
+            batch = r_candidates.index.to_numpy()[indices].tolist()
+        else:
+            raise NotImplementedError("Unknown mode for GPBatchUCB Agent")
 
-            if self.alpha == 'auto':
-                _t = i+max(len(self.seed_data) - len(self._initial_seed_indices), 1)
-                alpha = self._premultip*np.sqrt(2*np.log(len(self.candidate_data)*_t**2*np.pi**2/6/self._delta))
-                print(alpha)
-            else:
-                alpha = self.alpha
-            t_pred += unc * alpha
-            s = np.argmax(t_pred)
-
-            name = r_candidates.index.tolist()[s]
-            print(s, (pred[s]*y_std+y_m)[0], t_pred[s][0], unc[s], candidate_data['target'].loc[name])
-            batch.append(name)
-            print(batch, candidate_data.loc[batch]['target'])
-            r_seed = r_seed.append(r_candidates.loc[name])
-            r_y = np.append(r_y, np.array([pred[s]]).reshape(1, 1), axis=0)
-            r_candidates = r_candidates.drop(name)
-        gpflow.utilities.reset_cache_bijectors(m)
         return self.candidate_data.loc[batch]
