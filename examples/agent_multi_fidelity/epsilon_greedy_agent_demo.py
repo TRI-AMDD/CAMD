3c0df5df23df422a40dd55173fab41c9a3ab93e5
Ubuntu <ubuntu@ip-172-31-53-189.ec2.internal>
2021-04-26 00:26:35 +0000

    cleaned up epsilon greedy demo script

--- examples/agent_multi_fidelity/epsilon_greedy_agent_demo.py
+++ examples/agent_multi_fidelity/epsilon_greedy_agent_demo.py
@@ -1,5 +1,5 @@
 """
-This script demonstrate how to run a multi-fidelity campaign using epsilon-greedy agent. The campaign was run using bounday condition acquisition (where all the DFT data is available). The example dataset used in this demo is the bandgap dataset in this folder. Please see "generate_example_featurized_dataset" notebook on how we generate/featurized the dataset. 
+This script demonstrates how to run a multi-fidelity campaign using epsilon-greedy agent. The campaign was run using bounday condition acquisition (where all the DFT data is available in the seed data). The example dataset used in this demo is the bandgap dataset (located in the current folder). Please see "generate_example_featurized_dataset" notebook on how we generated/featurized the dataset.
 """
 
 import os
@@ -21,7 +21,7 @@ candidate_data = df.loc[df.expt_data==1]
 
 ####################################################
 # Create the folder where the results go
-os.system('rm -rf Results/epsilon_greedy') # If the folder alreadt exist, delete it first 
+os.system('rm -rf Results/epsilon_greedy') # If the folder alreadt exist, delete it first
 os.system('mkdir -p Results/epsilon_greedy')
 
 # Provide inputs for the campaign
@@ -32,15 +32,15 @@ model = SVR(C=10)
 
 ####################################################
 # Run the campaign
-agent = EpsilonGreedyMultiAgent(candidate_data=candidate_data, seed_data=seed_data, 
-                               features=data_features, target_prop='bandgap', target_prop_val=1.8, 
+agent = EpsilonGreedyMultiAgent(candidate_data=candidate_data, seed_data=seed_data,
+                               features=data_features, target_prop='bandgap', target_prop_val=1.8,
                                 model=model, total_budget=N_query, highFi_query_frac=1)
 experiment = ATFSampler(dataframe=candidate_data)
 analyzer = MultiAnalyzer(target_prop='bandgap', prop_range=[1.6, 2.0])
 
 with cd('Results/epsilon_greedy'):
-    campaign = Campaign(candidate_data=candidate_data, seed_data=seed_data, 
-                        agent=agent, experiment=experiment, 
+    campaign = Campaign(candidate_data=candidate_data, seed_data=seed_data,
+                        agent=agent, experiment=experiment,
                         analyzer=analyzer)
     campaign.auto_loop(n_iterations=iterations, initialize=True)
 
