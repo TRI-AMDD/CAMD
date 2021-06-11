303de7b666c5c41dc0a0f00cbbafbb7dccc53640
Joseph Montoya <joseph.montoya@tri.global>
2021-06-11 15:45:15 -0700

    v2021.6.11-post9 release

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ after the fact sampling of known data.
 setup(
     name='camd',
     url="https://github.com/TRI-AMDD/CAMD",
-    version="2021.6.11-post8",
+    version="2021.6.11-post9",
     packages=find_packages(),
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
