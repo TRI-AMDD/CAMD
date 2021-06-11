7006278ff2d1d55f44f7e974ed68da0941361bb0
Joseph Montoya <joseph.montoya@tri.global>
2021-06-11 15:14:22 -0700

    v2021.6.11-post1 release

--- camd/__init__.py
+++ camd/__init__.py
@@ -11,7 +11,7 @@ from functools import partial
 from tqdm import tqdm as _tqdm
 
 
-__version__ = "2021.6.11-post3"
+__version__ = "2021.6.11-post1"
 
 CAMD_ROOT = os.path.dirname(os.path.abspath(__file__))
 CAMD_TEST_FILES = os.path.join(CAMD_ROOT, "tests", "test_files")
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ after the fact sampling of known data.
 setup(
     name='camd',
     url="https://github.com/TRI-AMDD/CAMD",
-    version="2021.6.11-post3",
+    version="2021.6.11-post1",
     packages=find_packages(),
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
