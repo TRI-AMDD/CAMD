d641d8cb4a15b4e1c0240fb38cbe9d8dfbb72175
Weike Ye <weike.ye@tri.global>
2022-06-28 16:41:37 -0700

    add test

---
+++ camd/experiment/tests/test_atomate.py
@@ -0,0 +1,53 @@
+import unittest
+import os
+import numpy as np
+
+from pymatgen.util.testing import PymatgenTest
+from camd.experiment.dft import AtomateExperiment
+import pandas as pd
+from fireworks import LaunchPad
+
+TEST_DIR = os.path.dirname(__file__)
+ATOMATE_DFT_TESTS = np.all(
+    [os.path.isfile(os.path.join(TEST_DIR, i)) for i in
+     ['db.json', 'my_launchpad.yaml']])
+SKIP_MSG = "Long tests disabled, provide db.json, my_launchpad.yaml " \
+           "in {} to run long tests".format(TEST_DIR)
+
+
+class AtomateTest(PymatgenTest):
+    @unittest.skipUnless(ATOMATE_DFT_TESTS, SKIP_MSG)
+    def test_get(self) -> None:
+        good_silicon = PymatgenTest.get_structure("Si")
+        bad_silicon = good_silicon.copy()
+
+        # Add another site at the same position
+        bad_silicon.append("Si", [0.1, 0.1, 0.15])
+        bad_silicon.append("Si", [0.1, 0.333, 0.15])
+        self.assertEqual(len(bad_silicon), 4)
+        data = pd.DataFrame(
+            {"structure": {
+                "good": good_silicon,
+                "bad": bad_silicon
+            }
+            }
+        )
+        db_file = os.path.join(TEST_DIR, "db.json")
+        lpad_file = os.path.join(TEST_DIR, "my_launchpad.yaml")
+        lpad = LaunchPad.from_file(lpad_file)
+        lpad.auto_load()
+        experiment = AtomateExperiment(lpad,
+                                       db_file,
+                                       poll_time=30,
+                                       launch_from_local=False)
+        experiment.submit(data)
+        # status = experiment.monitor()
+        results = experiment.get_results()
+
+        self.assertAlmostEqual(results.loc['good', 'final_energy_per_atom'], -5.420645, 5)
+        self.assertIsNone(results.loc['bad', 'task_id'])
+        self.assertEqual(experiment.status, "COMPLETED")
+
+
+if __name__ == '__main__':
+    unittest.main()
